bdbd4a730340e02616f9a680c798f9a1b7c89cb1
anand <anand.peddakotla@gmail.com>
2024-05-09 23:10:40 -0400

    first commit

---
+++ src/javaPractice/ChildClass.java
@@ -0,0 +1,7 @@
+package javaPractice;
+
+public class ChildClass extends ParentClass {
+	int empID = 123;
+	String empName = "Child James";
+	
+}
---
+++ src/javaPractice/ParentClass.java
@@ -0,0 +1,7 @@
+package javaPractice;
+
+public class ParentClass {
+
+	int empID = 1234;
+	String empName = "Parent James";
+}
